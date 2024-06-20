8effabd97188c39bdfe5987eaa7f1a67e0882c97
Andres Alarcon <aalarcon@monoku.com>
2024-06-20 17:26:58 -0500

    chore: create homebrew install script with leaves and casks

---
+++ homebrew/install.sh
@@ -0,0 +1,76 @@
+# -------------------------
+# Leaves
+# -------------------------
+
+# Git and GitHub
+brew install git
+brew install gh
+brew install git-standup
+
+# Node and Package Managers
+brew install nvm
+brew install pnpm
+brew install yarn
+brew install pkgxdev/made/pkgx
+
+# Shell and Utilities
+brew install zsh
+brew install fzf
+brew install zoxide
+brew install stow
+brew install coreutils
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
+
+# Cloud and Deployment
+brew install awscli
+brew install flyctl
+brew install supabase/tap/supabase
+
+# Miscellaneous Utilities
+brew install jq
+brew install ffmpeg
+brew install fastfetch
+brew install tpm
+brew install act
+
+# -------------------------
+# Casks
+# -------------------------
+
+# Daily
+brew install --cask 1password
+brew install --cask alacritty
+brew install --cask slack-cli
+brew install --cask rectangle
+brew install --cask raycast
+
+# Browsers
+brew install --cask arc
+brew install --cask firefox
+brew install --cask google-chrome
+brew install --cask brave-browser
+
+# Development
+brew install --cask orbstack
+brew install --cask android-studio
+brew install --cask ngrok
+brew install --cask pgadmin4
+brew install --cask zulu17
+
+# Messaging
+brew install --cask signal
+brew install --cask discord
+brew install --cask zoom
+
+# Fonts
+brew install --cask font-hasklug-nerd-font
+brew install --cask font-lilex-nerd-font
+brew install --cask font-monaspace
+
+# Design
+figma
+
+# Misc
+brew install --cask sonos
+brew install --cask appcleaner
+brew install --cask surfshark
